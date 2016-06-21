46772e419c1902ffbd24a62130231c4c2ae9ce94
Eric Drechsel <eric@pdxhub.org>
2016-06-21 13:50:46 -0700

    Test skeleton
    Karma/Mocha/Chai with babel and jspm

---
+++ test/app.spec.js
@@ -0,0 +1,15 @@
+import AutocompleteDemo from 'app/autocomplete_demo';
+import ReactTestUtils from 'react/lib/ReactTestUtils';
+import React from 'react';
+
+// https://facebook.github.io/react/docs/test-utils.html
+// http://www.asbjornenge.com/wwc/testing_react_components.html
+
+describe('Autocomplete', function(){
+  it('says hello', function(){
+    let shallowRenderer = ReactTestUtils.createRenderer();
+    shallowRenderer.render(React.createElement(AutocompleteDemo));
+    let result = shallowRenderer.getRenderOutput();
+    expect(result.type).to.equal('h1');
+  });
+});
