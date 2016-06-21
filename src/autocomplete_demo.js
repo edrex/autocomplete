ebf41e71a14cffb2e1c69ff26ce01785280f539e
Eric Drechsel <eric@pdxhub.org>
2016-06-21 16:26:43 -0700

    implement boundsOfWordAtPosition function

--- src/autocomplete_demo.js
+++ src/autocomplete_demo.js
@@ -1,21 +1,42 @@
 import React from 'react';
+
+const wordCharsPat = '\\w',
+    startRegex = new RegExp(wordCharsPat + '*$'),
+    endRegex = new RegExp('^' + wordCharsPat + '*');
+
+export function boundsOfWordAtPosition(text, position) {
+  const textBefore = text.slice(0, position),
+    startMatch = startRegex.exec(textBefore),
+    start = startMatch.index;
+  // don't match at the start of a word
+  if (start == position) {
+    return [0, 0];
+  }
+  const textAfter = text.slice(position),
+    endMatch = endRegex.exec(textAfter),
+    end = position + endMatch[0].length;
+  return [start, end];
+}
+
 export default class AutoCompleteDemo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: props.initialValue};
+    this.state = {text: props.initialText};
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
-    this.setState({value: event.target.value});
+    const currentPosition = event.target.selectionEnd
+    const currentWord = event.target.value
+    this.setState({text: event.target.value});
     console.log(event);
   }
   render() {
     return React.createElement('textarea', {
       type: "text",
-      value: this.state.value,
+      value: this.state.text,
       onChange: this.handleChange
     });
   }
 }
 
-AutoCompleteDemo.defaultProps = {initialValue: 'Hello!'};
+AutoCompleteDemo.defaultProps = {initialText: 'Hello!'};
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,15 +1,30 @@
 import AutocompleteDemo from 'app/autocomplete_demo';
+import {boundsOfWordAtPosition} from 'app/autocomplete_demo';
 import ReactTestUtils from 'react/lib/ReactTestUtils';
 import React from 'react';
 
 // https://facebook.github.io/react/docs/test-utils.html
 // http://www.asbjornenge.com/wwc/testing_react_components.html
 
-describe('Autocomplete', function(){
-  it('starts with a textarea', function(){
-    let shallowRenderer = ReactTestUtils.createRenderer();
+describe('AutocompleteDemo', function(){
+  it('renders a textarea', function(){
+    const shallowRenderer = ReactTestUtils.createRenderer();
     shallowRenderer.render(React.createElement(AutocompleteDemo));
-    let result = shallowRenderer.getRenderOutput();
+    const result = shallowRenderer.getRenderOutput();
     expect(result.type).to.equal('textarea');
+  });
+});
+
+describe('boundsOfWordAtPosition', function(){
+  it('matches bounds of current word', function(){
+    const text = "fooo fooooo ffo-oooo";
+    for (const [pos, want] of [
+      [6, [5, 11]],
+      [0, [0, 0]],
+    ]) {
+      const have = boundsOfWordAtPosition(text, pos);
+      expect(have).to.deep.equal(want);
+      
+    }
   });
 });
