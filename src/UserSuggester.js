e2672775cd09c14ab5d856aef50ebfb9a63d6baa
Eric Drechsel <eric@pdxhub.org>
2016-06-24 18:25:04 -0700

    8 results (was 5)

--- src/UserSuggester.js
+++ src/UserSuggester.js
@@ -4,6 +4,6 @@ export default function UserSuggester(users){
   return (string) => {
     let r = new RegExp(escRegex(string), 'i')
     return users.filter(u => r.test(u.username) || r.test(u.name))
-      .slice(0,5);
+      .slice(0,8);
   }
 }
